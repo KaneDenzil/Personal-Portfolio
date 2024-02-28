3f46090ad77bf8a3d272ed7e0d150657d534de0c
Kane Bernard <kane.bernard@contractor.brightinsight.com>
2024-02-28 06:04:30 -0500

    Uninstalled React Router and React-Dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,29 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Home } from './Pages/Home.tsx';
-import { SamplePage } from './Pages/SamplePage.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/personal-portfolio/",
-    element: <App />,
-    children: [
-      {
-        path: "/personal-portfolio/",
-        element: <Home />
-      },
-      {
-        path: "/personal-portfolio/sample-page/",
-        element: <SamplePage />
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
